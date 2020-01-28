9557eb376704fd7dc5d931439463c22ce72b0a06
Shanavas Shaji <shanavascruise@gmail.com>
2020-01-28 21:41:15 +0200

    Added firebase messaging

--- createStore.js
+++ createStore.js
@@ -6,6 +6,7 @@ import { createStore, applyMiddleware } from 'redux'
 import { persistStore } from 'redux-persist';
 import { PersistGate } from 'redux-persist/integration/react'
 import { Provider } from 'react-redux';
+import {firebase} from '@react-native-firebase/messaging';
 
 import rootReducer from './src/reducers'
 import createSagaMiddleware from 'redux-saga';
@@ -25,7 +26,29 @@ const store = createStore(
 // Middleware: Redux Saga
 sagaMiddleware.run(rootSaga);
 
-const persistor = persistStore(store)
+const persistor = persistStore(store, null, async () => {
+  const messaging = firebase.messaging();
+  try {
+    const enabled = await messaging.hasPermission()
+    if (!enabled) {
+      await messaging.requestPermission()
+    }
+
+    const token = await messaging.getToken()
+    if (token) {
+      // store.dispatch(AppActions.updateFcmToken(token))
+      console.token('the token is ' + token);
+    }
+
+    messaging.onTokenRefresh((fcmToken) => {
+      // store.dispatch(AppActions.updateFcmToken(fcmToken))
+      console.token('the refreshed token is ' + fcmToken);
+    })
+  } catch (error) {
+    // TODO Decide on whether this error is important is enough to show to the user
+      console.log('error in fetching messaging', error)
+  }
+})
 
 export default function ReduxProvider(Component) {
     return (props) => (
